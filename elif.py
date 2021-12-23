ad187240300e1595ad93ee026d26d62686fe47a2
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2021-12-23 12:31:50 +0300

    Update elif.py

--- elif.py
+++ elif.py
@@ -1,12 +1,12 @@
-a=int(input("sayı giriniz:"))
-aile=["ertan","halime","aysun","ercan"]
+a=int(input("give a number:"))
+aile=["enes","seyma","kerem","munis"]
 if a==1:
-    print(aile[0])
+    print(family[0])
 elif a==2:
-    print(aile[1])
+    print(family[1])
 elif a==3:
-    print(aile[2])
+    print(family[2])
 elif a==4:
-    print(aile[3])
+    print(family[3])
 else:
-    print("1,2,3 veya 4 giriniz.")
+    print("You are wrong number. Please give a 1,2,3 or 4.")
